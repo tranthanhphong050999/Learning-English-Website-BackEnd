f7666bf143029cf92384e0b672f66b309ba1d9a6
phongle11303 <tranthanhphong050999@gmail.com>
2021-03-29 23:45:01 +0700

    xong update

--- DAO/AccountDAO.js
+++ DAO/AccountDAO.js
@@ -11,6 +11,14 @@ exports.getAllAccount = function(callbackQuery) {
         }
     })
 }
+exports.getOneAccountById = async function(AC_Id) {
+    return new Promise(resolve => {
+        var sql = "select * from account where AC_Id=?";
+        database.connection.query(sql, [AC_Id], function(err, results, fields) {
+            resolve(results)
+        })
+    })
+}
 exports.getOneAccount = async function(userName, passWord) {
     return new Promise(resolve => {
 
@@ -37,4 +45,49 @@ exports.getOneAccount = async function(userName, passWord) {
 
     });
 
+}
+
+exports.addAccount = async function(AC_userName, AC_fullName, AC_Email, AC_Streak, AC_Exp, AC_State, AC_Role, AC_idExpOfOneDay, AC_passWord) {
+    return new Promise(resolve => {
+        var sql = "INSERT INTO account (AC_userName,AC_fullName,AC_Email,AC_Streak,AC_Exp, AC_State,AC_Role,AC_idExpOfOneDay,AC_passWord) VALUES ?";
+        var values = [
+            [AC_userName, AC_fullName, AC_Email, parseInt(AC_Streak), parseInt(AC_Exp), parseInt(AC_State), parseInt(AC_Role), parseInt(AC_idExpOfOneDay), AC_passWord]
+        ]
+        database.connection.query(sql, [values], function(err, results, fields) {
+            if (err) {
+                resolve(err)
+            } else {
+                resolve("true")
+            }
+
+        })
+    })
+}
+exports.updateAccount = async function(AC_Id, AC_userName, AC_fullName, AC_Email, AC_Streak, AC_Exp, AC_State, AC_Role, AC_idExpOfOneDay, AC_passWord) {
+    return new Promise(resolve => {
+        var sql = "UPDATE account SET AC_userName=?,AC_fullName=?,AC_Email=?,AC_Streak=?,AC_State=?,AC_Role=?,AC_idExpOfOneDay=?,AC_idExpOfOneDay=?,AC_passWord=?  Where AC_Id=?"
+        var values = [AC_userName, AC_fullName, AC_Email, parseInt(AC_Streak), parseInt(AC_Exp), parseInt(AC_State), parseInt(AC_Role), parseInt(AC_idExpOfOneDay), AC_passWord, AC_Id]
+
+        database.connection.query(sql, values, function(err, results, fields) {
+            if (err) {
+                resolve(err)
+            } else {
+                resolve("true")
+            }
+
+        })
+    })
+}
+exports.deleteAccountById = async function(AC_Id) {
+    return new Promise(resolve => {
+        var sql = "delete from account where AC_Id=?";
+        database.connection.query(sql, [AC_Id], function(err, results, fields) {
+            if (err) {
+                resolve("false")
+            } else {
+                resolve("true")
+            }
+
+        })
+    })
 }
--- index.js
+++ index.js
@@ -8,19 +8,24 @@ app.listen(5000);
 app.use(cors())
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-/*app.get("/word",function(request,response){
-//response.send('<h1 style="color:red;"> Phong lê </h1>');
-const a="0";
-database.getall(a);
-});*/
-app.get("/account", function(request, response) {
-    //response.send('<h1 style="color:red;"> Phong lê </h1>');
+//*********ACCOUNT */
+
+app.get("/account/getall", function(request, response) {
+
     accountDao.getAllAccount(function(resultQuery) {
         response.json(resultQuery);
     });
 });
+app.get("/account/getone/:id", async function(request, response) {
+    var AC_Id = request.params.id;
+    try {
+        var temp = await accountDao.getOneAccountById(AC_Id)
+        response.json(temp)
+    } catch (error) {
 
-app.post("/word", urlencodedParser, async function(request, response) {
+    }
+})
+app.post("/account/login", urlencodedParser, async function(request, response) {
     var username = request.body.username;
     var password = request.body.password;
     try {
@@ -33,8 +38,54 @@ app.post("/word", urlencodedParser, async function(request, response) {
 
     //response.json(temp);
 });
+app.post("/account/add", urlencodedParser, async function(request, response) {
+    var AC_userName = request.body.AC_userName;
+    var AC_fullName = request.body.AC_fullName;
+    var AC_Email = request.body.AC_Email;
+    var AC_Streak = (request.body.AC_Streak != "") ? request.body.AC_Streak : 0;
+    var AC_Exp = (request.body.AC_Exp != "") ? request.body.AC_Exp : 0;
+    var AC_State = (request.body.AC_State != "") ? request.body.AC_State : 0;
+    var AC_Role = (request.body.AC_Role != "") ? request.body.AC_Role : 0;
+    var AC_idExpOfOneDay = (request.body.AC_idExpOfOneDay != "") ? request.body.AC_idExpOfOneDay : 0;
+    var AC_passWord = request.body.AC_passWord;
+    try {
+        var temp = await accountDao.addAccount(AC_userName, AC_fullName, AC_Email, AC_Streak, AC_Exp, AC_State, AC_Role, AC_idExpOfOneDay, AC_passWord)
+        response.send(temp);
+    } catch (error) {
+        console.log(error)
+    }
 
+});
 
+app.get("/account/delete/:id", async function(request, response) {
+    var AC_Id = request.params.id;
+    try {
+        var temp = await accountDao.deleteAccountById(AC_Id)
+        response.send(temp)
+    } catch (error) {
+
+    }
+})
+
+app.post("/account/update", urlencodedParser, async function(request, response) {
+    var AC_Id = request.body.AC_Id;
+    var AC_userName = request.body.AC_userName;
+    var AC_fullName = request.body.AC_fullName;
+    var AC_Email = request.body.AC_Email;
+    var AC_Streak = (request.body.AC_Streak != "") ? request.body.AC_Streak : 0;
+    var AC_Exp = (request.body.AC_Exp != "") ? request.body.AC_Exp : 0;
+    var AC_State = (request.body.AC_State != "") ? request.body.AC_State : 0;
+    var AC_Role = (request.body.AC_Role != "") ? request.body.AC_Role : 0;
+    var AC_idExpOfOneDay = (request.body.AC_idExpOfOneDay != "") ? request.body.AC_idExpOfOneDay : 0;
+    var AC_passWord = request.body.AC_passWord;
+    try {
+        var temp = await accountDao.updateAccount(AC_Id, AC_userName, AC_fullName, AC_Email, AC_Streak, AC_Exp, AC_State, AC_Role, AC_idExpOfOneDay, AC_passWord)
+        response.send(temp);
+    } catch (error) {
+        console.log(error)
+    }
+
+});
 app.get("/word/:id", function(request, response) {
     var id = request.params.id;
     response.send('<h1 style="color:red;"> Phong lê' + id + ' </h1>');
